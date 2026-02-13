bdf44ee660a5c180b4e6117f8200aa6587d3c2e6
htlemos <150178195+htlemos@users.noreply.github.com>
2026-02-13 13:51:23 +0000

    Create const.py

---
+++ custom_components/keeplink_switch/const.py
@@ -0,0 +1,9 @@
+"""Constants for the Keeplink Switch integration."""
+
+DOMAIN = "keeplink_switch"
+CONF_HOST = "host"
+CONF_USERNAME = "username"
+CONF_PASSWORD = "password"
+
+# Default update interval (1 minute)
+DEFAULT_SCAN_INTERVAL = 60
